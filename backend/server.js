b84f1ea58cae28b6fdf704a42ee8d9677231252a
shalman <shalman4502n@gmail.com>
2025-11-18 00:27:49 +0530

    sendGrid integration , SMTP removed , render.com SMTP blocking error resolved

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from 'express';
 
 import helmet from 'helmet';
 import cors from 'cors';
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,19 @@
-import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+import sgMail from "@sendgrid/mail";
 
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async ({ to, subject, text, html }) => {
-  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-    throw new Error('Email credentials not configured');
-  }
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
-});
-  const mailOptions = {
-    from: `"Stranger Things Portal" <${process.env.EMAIL_USER}>`,
+  const msg = {
     to,
+    from: "shalman4502n@gmail.com",
     subject,
     text,
-    html
+    html,
   };
 
-  await transporter.sendMail(mailOptions);
+  await sgMail.send(msg);
 };
 
 export default sendEmail;
